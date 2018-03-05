6cfc6bd13ee7fee53960b4ade3fdbd19e8326d2e
joelworsham <joel.worsham@gmail.com>
2018-03-05 11:58:03 -0500

    Add page title to full width page template

--- page-templates/page-full-width.php
+++ page-templates/page-full-width.php
@@ -7,6 +7,11 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 
 <div class="main-wrap full-width">
+    <h1 class="entry-title banner-title">
+		<?php the_title(); ?>
+		<?php kwaske_banner_end(); ?>
+    </h1>
+
 	<main class="main-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'template-parts/content', 'page' ); ?>
